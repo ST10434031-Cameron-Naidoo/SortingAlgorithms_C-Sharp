706c60b181b7203b04ba34acf330b45062e1897c
ST10434031-Cameron-Naidoo <st10434031@myemeris.edu.za>
2026-07-16 22:01:44 +0200

    added BubSort static class

---
+++ SortingAlgorithms/Program.cs
@@ -0,0 +1,56 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        int count = 0;
+        int[] intArray = { 5, 8, 2, 9, 1 };
+
+        SortingAlgorithms.Print(intArray);
+        Console.WriteLine();
+
+        SortingAlgorithms.BubSort(intArray);
+
+        SortingAlgorithms.Print(intArray);
+
+    }
+
+
+
+}
+
+public static class SortingAlgorithms
+{
+    public static void BubSort<T>(T[] array) where T : IComparable
+    {
+        for (int i = 0; i < array.Length; i++) {
+            {
+                for (int j = 0; j < array.Length - 1; j++)
+                {
+                    if (array[j].CompareTo(array[j + 1]) > 0)
+                    {
+                        T temp = array[j];
+                        array[j] = array[j + 1];
+                        array[j + 1] = temp;
+                    }
+                }
+            }
+        }
+    }
+
+    public static void Print<T>(T[] array) {
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (i != array.Length - 1)
+                {
+                    Console.Write(array[i] + ",");
+                }
+                else
+                {
+                    Console.WriteLine(array[i]);
+                }
+            }
+
+        }
+    } 
+}
